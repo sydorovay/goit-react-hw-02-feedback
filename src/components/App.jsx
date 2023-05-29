d8860466ed049439935f630e7383ee279197a13a
Artem Sydorov <sydorovay@gmail.com>
2023-05-29 12:21:46 +0200

    create Feedback component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,6 @@
 import React, { Component } from 'react';
 import Statistics from './statistics/Statistics';
-
-
-
-const FeedbackOptions = ({ options, onLeaveFeedback }) => (
-  <div>
-    {options.map(option => (
-      <button key={option} onClick={() => onLeaveFeedback(option)}>
-        {option}
-      </button>
-    ))}
-  </div>
-);
+import FeedbackOptions from './feedbackOptions/FeedbackOptions';
 
 const Section = ({ title, children }) => (
   <div>
